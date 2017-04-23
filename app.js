0344cad0621b774889846b4ad8439c43ba2e288a
Martin R. Hristov <martin.r.hristov@sap.com>
2017-04-23 15:24:45 +0300

    backend bootstrap almost done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 require('./routes/auth')(app);
 app.use(express.static('public'));
@@ -11,3 +12,16 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
+
+mongoose.connect('mongodb://localhost/27017');
+
+var db = mongoose.connection;
+
+var kittySchema = mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({ name: 'Silence' });
+console.log(silence.name, 'wtffffff'); // 'Silence'
